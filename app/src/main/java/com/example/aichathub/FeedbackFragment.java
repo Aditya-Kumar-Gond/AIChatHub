79638d2138846d708cb448335ec5dead372ce6d4
Aditya Kumar Gond <adityagondbg@gmail.com>
2024-04-06 11:55:09 +0530

    Profile and feedback added

---
+++ app/src/main/java/com/example/aichathub/FeedbackFragment.java
@@ -0,0 +1,21 @@
+package com.example.aichathub;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FeedbackFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_feedback, container, false);
+        return v;
+    }
+
+}
