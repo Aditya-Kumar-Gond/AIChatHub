d0ede34cda42ff3adc8196d0ea1a1c830494460d
Aditya Kumar Gond <adityagondbg@gmail.com>
2024-04-10 21:01:35 +0530

    signup added

---
+++ app/src/main/java/com/example/aichathub/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.example.aichathub;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_activity);
+    }
+}
