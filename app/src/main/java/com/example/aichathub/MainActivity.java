cfc55f0e7f80bf29a12bc14f994324862d5ab1ab
Aditya Kumar Gond <adityagondbg@gmail.com>
2024-04-10 13:54:29 +0530

    Profile and feedback added2

--- app/src/main/java/com/example/aichathub/MainActivity.java
+++ app/src/main/java/com/example/aichathub/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.aichathub;
 
+import static android.graphics.Color.BLACK;
 import static android.graphics.Color.WHITE;
 
 import androidx.annotation.NonNull;
@@ -10,6 +11,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -53,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         profile_img = findViewById(R.id.circleProfile_image);
         txt_header_feedback = findViewById(R.id.header2);
 
-
+        navigationHeader(navi);
         navigation_icon.setOnClickListener(v->{
             drawerLayout.openDrawer(GravityCompat.START);
         });
@@ -112,35 +114,120 @@ public class MainActivity extends AppCompatActivity {
             profile_bottom_icon.setColorFilter(getResources().getColor(R.color.purple_dark));
         });
 
-        navigationHeader(navi);
+
 
     }
-
+    LinearLayout home,setting,refer,feedback,logout;
+    TextView home_tv,setting_tv,refer_tv,feedback_tv;
+    ImageView home_icon,setting_icon,refer_icon,feedback_icon;
+    TextView username,email;
+    @SuppressLint("UseCompatLoadingForDrawables")
     private void navigationHeader(ViewGroup viewGroup) {
         View view = LayoutInflater.from(this).inflate(R.layout.nav_header,viewGroup,false);
-        LinearLayout home,setting,refer,support,logout;
-        TextView username,email;
+        navi.addHeaderView(view);
+
         Button edit_btn;
-        ImageView profile_img;
+        ImageView profile_img_header;
         home = view.findViewById(R.id.home_header);
         setting = view.findViewById(R.id.setting_header);
         refer = view.findViewById(R.id.refer_header);
-        support = view.findViewById(R.id.support_header);
+        feedback = view.findViewById(R.id.support_header);
         logout = view.findViewById(R.id.logout_header);
+        home_tv = view.findViewById(R.id.home_text);
+        setting_tv = view.findViewById(R.id.setting_text);
+        refer_tv = view.findViewById(R.id.refer_text);
+        feedback_tv = view.findViewById(R.id.feedback_text);
+        home_icon = view.findViewById(R.id.home_icon);
+        setting_icon = view.findViewById(R.id.setting_icon);
+        refer_icon = view.findViewById(R.id.refer_icon);
+        feedback_icon = view.findViewById(R.id.phone_icon);
+
         username = view.findViewById(R.id.username_header);
         email = view.findViewById(R.id.user_email_header);
         edit_btn = view.findViewById(R.id.edit_profile_btn_header);
-        profile_img = view.findViewById(R.id.user_image_header);
+        profile_img_header = view.findViewById(R.id.user_image_header);
+
+        feedback.setOnClickListener(v->{
+            setAppearanceOfHolder();
+            v.setBackground(getResources().getDrawable(R.drawable.purple_bg_holder));
+            feedback_tv.setTextColor(WHITE);
+            feedback_icon.setColorFilter(WHITE);
+
+            FeedbackFragment fragment = new FeedbackFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.fragment,fragment).commit();
+            profile_img.setVisibility(View.VISIBLE);
+            txt_header.setVisibility(View.GONE);
+            txt_header_feedback.setVisibility(View.VISIBLE);
+            txt_header_feedback.setText("Feedback");
+            feedback_bottom_icon.setBackground(getDrawable(R.drawable.bottom_selection_bg));
+            dashBoard_bottom_icon.setBackground(null);
+            profile_bottom_icon.setBackground(null);
+            feedback_bottom_icon.setColorFilter(getResources().getColor(R.color.white));
+            dashBoard_bottom_icon.setColorFilter(getResources().getColor(R.color.purple_dark));
+            profile_bottom_icon.setColorFilter(getResources().getColor(R.color.purple_dark));
+            drawerLayout.close();
+        });
 
         logout.setOnClickListener(v->{
             Toast.makeText(MainActivity.this,"logout",Toast.LENGTH_SHORT).show();
         });
 
         edit_btn.setOnClickListener(v->{
+            drawerLayout.close();
             profileFragment fragment = new profileFragment();
             FragmentManager manager = getSupportFragmentManager();
             manager.beginTransaction().replace(R.id.fragment,fragment).commit();
+            profile_img.setVisibility(View.GONE);
+            txt_header.setVisibility(View.VISIBLE);
+            txt_header_feedback.setVisibility(View.GONE);
+            profile_bottom_icon.setBackground(getDrawable(R.drawable.bottom_selection_bg));
+            dashBoard_bottom_icon.setBackground(null);
+            feedback_bottom_icon.setBackground(null);
+            profile_bottom_icon.setColorFilter(getResources().getColor(R.color.white));
+            dashBoard_bottom_icon.setColorFilter(getResources().getColor(R.color.purple_dark));
+            feedback_bottom_icon.setColorFilter(getResources().getColor(R.color.purple_dark));
         });
 
+        home.setOnClickListener(v->{
+            setAppearanceOfHolder();
+            v.setBackground(getResources().getDrawable(R.drawable.purple_bg_holder));
+            home_tv.setTextColor(WHITE);
+            home_icon.setColorFilter(WHITE);
+            drawerLayout.close();
+            DashboardFragment fragment = new DashboardFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.fragment,fragment).commit();
+            profile_img.setVisibility(View.VISIBLE);
+            txt_header.setVisibility(View.GONE);
+            txt_header_feedback.setVisibility(View.VISIBLE);
+            txt_header_feedback.setText("Dashboard");
+            dashBoard_bottom_icon.setBackground(getDrawable(R.drawable.bottom_selection_bg));
+            feedback_bottom_icon.setBackground(null);
+            profile_bottom_icon.setBackground(null);
+            dashBoard_bottom_icon.setColorFilter(getResources().getColor(R.color.white));
+            feedback_bottom_icon.setColorFilter(getResources().getColor(R.color.purple_dark));
+            profile_bottom_icon.setColorFilter(getResources().getColor(R.color.purple_dark));
+        });
+
+    }
+
+    private void setAppearanceOfHolder() {
+        home.setBackground(null);
+        setting.setBackground(null);
+        refer.setBackground(null);
+        feedback.setBackground(null);
+        logout.setBackground(null);
+
+        home_tv.setTextColor(Color.BLACK);
+        setting_tv.setTextColor(Color.BLACK);
+        refer_tv.setTextColor(Color.BLACK);
+        feedback_tv.setTextColor(Color.BLACK);
+
+        home_icon.setColorFilter(Color.BLACK);
+        setting_icon.setColorFilter(Color.BLACK);
+        refer_icon.setColorFilter(Color.BLACK);
+        refer_icon.setColorFilter(Color.BLACK);
+        feedback_icon.setColorFilter(BLACK);
     }
 }
